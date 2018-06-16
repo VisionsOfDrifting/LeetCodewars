777441cfa57515dab434ab6909a6194190dca50e
Nicholas <nhpappas@ucsc.edu>
2018-06-16 01:28:21 -0700

    Forgot a description, cleaning up code

--- twoSum.py
+++ twoSum.py
@@ -1,12 +1,20 @@
-#Add the ability for script inputed nums and target
-#May want to pseudo random generate test sets
+""" Problem:
+    Given an array (list) of ints, return indicies of the
+    two numbers such that they add up to a specific target.
+    Solution: 
+    Walk through the array (list) and compare target minus
+    current value with a hash-map (dictionary) of values 
+    to indicies. If a match is found return it's indicies 
+    otherwise return "No Match".
+"""
+# Add the ability for script inputed nums and target
+# May want to pseudo random generate test sets
 
 def twoSum(nums, D, target):
    if(len(nums) == 1):
-      print("No Match")
-      return 
+      return "No Match"
    x = nums[0]
-   print("nums = ",nums)#,"D = ",D,"x = ",x)
+   print("nums = ",nums)
    if(target-x in D):
       print("Match Found!")
       print(x," + ",target-x," = ",target)
@@ -17,6 +25,6 @@ nums = [1, 7, 11, 15]
 print(nums)
 D = dict(zip(nums, range(len(nums))))
 print(D)
-target = 22
+target = 23
 print("target = ", target)
 print(twoSum(nums, D, target))
