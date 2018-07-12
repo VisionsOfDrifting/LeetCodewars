56a37a797ae16c044e58b55da810441e025ba9f1
Nick <nickhtpappas@gmail.com>
2018-07-12 00:07:12 -0700

    I saw another solution on pastebin for this. The guy straight up cheated. Didn't even remake the same tree. What has compsci come too?

--- Python/binaryTree1.py
+++ Python/binaryTree1.py
@@ -1,3 +1,21 @@
+"""Problem: 
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
+and deserialize(s), which deserializes the string back into the tree.
+
+For example, given the following Node class
+class Node:
+    def init(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+The following test should pass:
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+Note: Its good to remember that preorder tree walks are for copying/ making trees
+and postorder tree walks are for deleting trees.
+"""
+
 class Node:
    def __init__(self, val, left=None, right=None):
       self.val = val
@@ -7,44 +25,44 @@ class Node:
 def serialize(root):
    s = ""
    s += root.val
-   if(not root.left):
+   if(root.left != None):
       s += "-"
       s += serialize(root.left)
-   if(not root.right):
+   if(root.right != None):
       s += "-"
       s += serialize(root.right)
    return s
    
 def deserialize(s):
    def deserializeHelper(node,nodes):
-      val = nodes.pop()
-      if not val:
-         return
+      val = nodes.pop(0)
+      print(nodes)
+      print(val)
       node = Node(val)
-      temp = nodes[len(nodes)-1]
-      sub = temp.split(".")
-      if(sub == "left"):
-         node.left = deserializeHelper(node.left,nodes)
-      temp = nodes[len(nodes)-1]
-      sub = temp.split(".")
-      if(sub == "right"):
-         node.right = deserializeHelper(node.left,nodes)
-      return node
-      
-      """temp = sub.split(".")
-      #left,left \/ = 1
-      if temp[len(temp)-1] == "left":
-         node = Node("""
-   nodes = s.split("-")
-   #root,left,left.left,right pre
-   #left.left,left,root,right in
-   #left.left,left,right,root post
-   for i in range(len(nodes)):
-      deserializeHelper(nodes[i])
-      
+      if(nodes != []):
+         temp = nodes[0]
+         print(temp)
+         sub = temp.split(".")
+         print(sub)
+         if(sub[len(sub)-1] == "left"):
+            node.left = deserializeHelper(node.left,nodes)
+      if(nodes != []):
+         temp = nodes[len(nodes)-1]
+         sub = temp.split(".")
+         if(sub[len(sub)-1] == "right"):
+            node.right = deserializeHelper(node.right,nodes)
+      return node      
+   nodes = s.split("-")   
+   print(nodes)
+   return deserializeHelper(None,nodes)
 
 node = Node('root', Node('left', Node('left.left')), Node('right'))
 print(serialize(node))
+print(deserialize(serialize(node)).left.left.val == 'left.left')
 #assert deserialize(serialize(node)).left.left.val == 'left.left'
 
-#I couldn't get this one in time.
+#I couldn't get this one in time. 
+
+#root,left,left.left,right pre
+#left.left,left,root,right in
+#left.left,left,right,root post
